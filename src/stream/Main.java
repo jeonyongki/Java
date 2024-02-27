069dbb44bdd70248dc377a2e9ad00d489c26ac49
jeonyongki <ygygjj@naver.com>
2024-02-27 12:18:39 +0900

    20240227

--- src/stream/Main.java
+++ src/stream/Main.java
@@ -3,11 +3,11 @@ public class Main{
 public static void main(String[] args)throws IOException{
 FileInputStream fis = new FileInputStream("d:/work/java/text/test.txt");
 
-// System.out.println(fis.read());//65
-// System.out.println(fis.read());//13
-// System.out.println(fis.read());//10
-// System.out.println(fis.read());//66
-// System.out.println(fis.read());//-1
+// System.out.println(fis.read());//65 대문자 'A'
+// System.out.println(fis.read());//13 캐리지 리턴 (Carriage Return, '\r')
+// System.out.println(fis.read());//10 줄 바꿈 (Line Feed, '\n')
+// System.out.println(fis.read());//66 대문자 'B'
+// System.out.println(fis.read());//-1 파일의 끝 (End of File, EOF)
 byte[] array = new byte[2];
 int n1 = fis.read(array);
 int n2 = fis.read(array);
